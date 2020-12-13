f4c2631e1b9c57cdece340d5fb5d69d956271bb1
SLILAM Anwar <slilamanouar@gmail.com>
2020-12-13 15:37:25 +0100

    initial commit

---
+++ src/java/webservice/CalculatriceWS.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webservice;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Pavilion
+ */
+@WebService(serviceName = "CalculatriceWS")
+public class CalculatriceWS {
+
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) {
+        //TODO write your implementation code here:
+        return i + j;
+    }
+
+
+    
+    
+}
